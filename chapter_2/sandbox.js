7120d1198d3edbd90c60d192a50f94ef1bad292e
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-21 12:09:40 +0000

    variables

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,35 @@
+// alert("hello hamed");
+// console.log(1);
+// console.log(2);
+
+
+//the value of the variable is changable
+let age = 25;
+let year = 2024
+
+console.log(age, year);
+
+age = 30
+console.log(age);
+
+//unchangable value of the variable
+
+const points = 100;
+
+console.log(points);
+
+// points = 101;
+// console.log(points);
+
+//older method to create variables and it s changable
+var score = 75;
+console.log(score);
+score = 76;
+console.log(score);
+
+
+
+
+
+
+
