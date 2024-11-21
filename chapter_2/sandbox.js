13a343662e4a2defaeb8cb8b8327ee0323f5cb8a
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-21 12:31:59 +0000

    string data type

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -27,9 +27,42 @@ console.log(score);
 score = 76;
 console.log(score);
 
+// my age should be myAge ,
+// variables cant start with number
+// , we cant use reserved key word for the
+// name of variables like let const = 20;
 
 
+//Data type 
 
+
+//Strings 
+console.log("hello,world!");
+
+let email = "hamedmiri71@gmail.com";
+console.log(email);
+
+//string concatenation
+
+let first_name = "Hamed";
+let last_name = "Miri";
+let full_name = first_name+ " " + last_name;
+console.log(full_name);
+
+//getting characters
+console.log(full_name[0]);
+
+//string length
+console.log(full_name.length);
+
+//string methods
+console.log(full_name.toUpperCase());
+
+let result = full_name.toLowerCase();
+console.log(result, full_name);
+
+let index = email.indexOf('@'); // index of the character in the string 
+console.log(index);
 
 
 
