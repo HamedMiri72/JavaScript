ae64585d2b7b351c687e7f1031fe64c36c56c7d7
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-22 23:48:27 +0000

    finctions

--- chapter_4/sandbox4.js
+++ chapter_4/sandbox4.js
@@ -1,5 +1,20 @@
-//Functions 
+//Functions decleration
 
 function greet(){
-    
+    console.log("hello world");
 }
+
+//function expression when we store function inside the variable
+
+const speak = function(name, time){
+    console.log(`good ${time} ${name}`);
+};
+
+
+greet();
+speak("hamed", "morning");
+
+
+
+
+
