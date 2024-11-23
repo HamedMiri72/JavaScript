58c6ca7f10af616e5524aa7a035f3aa0b0721d19
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-23 00:00:43 +0000

    arrow function

--- chapter_4/sandbox4.js
+++ chapter_4/sandbox4.js
@@ -15,6 +15,37 @@ greet();
 speak("hamed", "morning");
 
 
+const calcArea = function(radius){
+    // let area = 3.14 * radius**2;
+    // return area;
+    return 3.14 * radius**2;
+}
+
+const area = calcArea(5); // global scope
+console.log(area);
+
+const calcVol = function(area){
+    return area **2;
+}
+
+const volum = calcVol(area);
+console.log(volum);
+
+
+//arrow function
+
+const calArea = (radius) => {
+    return 3.14 * radius**2;
+};
+
+
+
+
+
+
+
+
+
 
 
 
